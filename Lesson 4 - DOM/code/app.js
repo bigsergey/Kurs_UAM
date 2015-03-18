47f3ff37b3fa8a6ba818aa0baff23375605a007f
Sergey Bolshov <bolszow@gmail.com>
2015-03-18 09:32:26 +0100

    Lession 4 - Exercise DOM

--- Lesson 4 - DOM/code/app.js
+++ Lesson 4 - DOM/code/app.js
@@ -1,5 +1,41 @@
-window.addEventListener('DOMContentLoaded', function () {
-	//When the page structure is loaded...
+window.addEventListener('DOMContentLoaded', function() {
+    //When the page structure is loaded...
+    $weight = document.querySelector('.weight > input');
+    $height = document.querySelector('.height > input');
 
+    $weightSpan = document.querySelector('.weight > span');
+    $heightSpan = document.querySelector('.height > span');
+
+    $bmi = document.querySelector('h1 > span');
+    $preferredHeight = document.querySelector('h2 > span');
+
+    var updateBMI = function() {
+        var bmi = $weight.value / ($height.value * $height.value * 0.0001);
+        $bmi.textContent = bmi;
+        switch (true) {
+            case (bmi >= 19 && bmi <= 25):
+                $bmi.setAttribute('class', 'ok');
+                break;
+            case (bmi >= 17 && bmi <= 30):
+                $bmi.setAttribute('class', 'medium');
+                break;
+            default:
+                $bmi.setAttribute('class', 'bad');
+                break;
+        }
+
+        $preferredHeight.textContent = Math.round(Math.sqrt($weight.value / 22) * 100);
+    };
+
+    $weight.addEventListener('change', function(event) {
+        $weightSpan.textContent = $weight.value;
+        updateBMI();
+    });
+
+    $height.addEventListener('change', function(event) {
+        $heightSpan.textContent = $height.value;
+        updateBMI();
+    });
+
+    updateBMI();
 });
-
