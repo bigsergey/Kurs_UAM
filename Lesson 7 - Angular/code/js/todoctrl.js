5f67ef290bd4a7016291bcf49fd3ee7ee33f0e93
Sergey Bolshov <bolszow@gmail.com>
2015-04-14 14:45:21 +0200

    Lession 7 - exercise

--- Lesson 7 - Angular/code/js/todoctrl.js
+++ Lesson 7 - Angular/code/js/todoctrl.js
@@ -1,15 +1,32 @@
-angular.module('myapp', []).controller('TodoCtrl', function ($scope) {
-	$scope.elements = [];
-	$scope.inputValue = '';
+angular.module('myapp', []).controller('TodoCtrl', function($scope) {
+    $scope.elements = [];
+    $scope.inputValue = '';
 
-	$scope.addNew = function () {
-		if (!$scope.inputValue) {
-			return;
-		}
-		$scope.elements.push({
-			value: $scope.inputValue,
-			status: true
-		});
-		$scope.inputValue = '';
-	};
+    $scope.addNew = function() {
+        if (!$scope.inputValue) {
+            return;
+        }
+        $scope.elements.push({
+            value: $scope.inputValue,
+            status: true
+        });
+        $scope.inputValue = '';
+        $scope.updateCounter();
+    };
+    $scope.remove = function(index) {
+    	$scope.elements.splice(index, 1);
+    	$scope.updateCounter();
+    };
+
+    $scope.updateCounter = function () {
+    	$scope.active = 0;
+    	$scope.inactive = 0;
+    	$scope.elements.forEach(function (val) {
+    		if (val.status) {
+    			$scope.inactive++;
+    		}
+    	});
+    	$scope.active = $scope.elements.length - $scope.inactive;
+    };
+    $scope.updateCounter();
 });
