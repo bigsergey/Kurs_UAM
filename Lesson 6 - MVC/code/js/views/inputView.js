0f1b0a7952e7810808ebabb12ba1fe5b12d839a7
Sergey Bolshov <bolszow@gmail.com>
2015-04-14 15:35:07 +0200

    add task when user press enter

--- Lesson 6 - MVC/code/js/views/inputView.js
+++ Lesson 6 - MVC/code/js/views/inputView.js
@@ -4,13 +4,23 @@ UAM.InputView = function(element) {
     this.textInput = element.querySelector('input');
     this.addButton = element.querySelector('.add-button');
 
-    this.addButton.onclick = function () {
-		_this.emit('addNewTaskClicked', (_this.textInput.value || 'New Task'));
-		_this.textInput.value = '';
+    var addNewTask = function() {
+        _this.emit('addNewTaskClicked', (_this.textInput.value || 'New Task'));
+        _this.textInput.value = '';
     };
+
+    this.addButton.onclick = function() {
+        addNewTask();
+    };
+
+    this.textInput.onkeypress = function(e) {
+        e = e || window.event;
+        if (e.keyCode == 13) {
+            addNewTask();
+        }
+    }
 
     UAM.EventEmitter.call(this);
 };
 
 UAM.utils.inherits(UAM.EventEmitter, UAM.InputView);
-
